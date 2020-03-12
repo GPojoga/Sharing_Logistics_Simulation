7de4ccaa9ba043683a972672acf57483babc6bd0
AntoninThioux <antonin.thioux@gmail.com>
2020-03-12 19:01:07 +0100

    made some style changes

--- application/src/components/VehicleSelector.vue
+++ application/src/components/VehicleSelector.vue
@@ -1,6 +1,7 @@
 <template>
     <div id="vehicleSelector">
-        <p align="left">Type of vehicles</p>
+        <br>
+        <p id="vehicleSelectorTitle">Type of vehicles</p>
         <div>
             <VehicleSelectorEntry vehicleDetails="Light Duty Van"></VehicleSelectorEntry>
             <VehicleSelectorEntry vehicleDetails="Heavy Duty Truck"></VehicleSelectorEntry>
@@ -19,5 +20,15 @@
 </script>
 
 <style scoped>
+    #vehicleSelector {
+        background-color: white;
+    }
 
+    #vehicleSelectorTitle {
+        text-align: left;
+        color: #007FEB;
+        font-family: "Arial", Arial, sans-serif;
+        font-weight: bold;
+        font-size: 90%;
+    }
 </style>
--- application/src/components/VehicleSelectorEntry.vue
+++ application/src/components/VehicleSelectorEntry.vue
@@ -1,8 +1,12 @@
 <template>
     <div>
-        <p align="left">
-            {{vehicleDetails}}
-            <input type="checkbox" v-model="isSelected">
+        <p id="entryText">
+            <label id="entryContainer">
+                {{vehicleDetails}}
+                <input type="checkbox" v-model="isSelected">
+                <span id="entryCheckbox"></span>
+            </label>
+
             <template v-if="isSelected">
                 <input type="number" v-model="lastInputQuantity" min="0" oninput="this.value = Math.abs(this.value)">
             </template>
@@ -29,5 +33,44 @@
 </script>
 
 <style scoped>
+    #entryText {
+        text-align: left;
+        color: #007FEB;
+        font-family: "Arial", Arial, sans-serif;
+        font-weight: bold;
+        font-size: 90%;
+    }
 
+    #entryContainer {
+        position: relative;
+        cursor: pointer;
+    }
+
+    /* Hide the default checkbox */
+    #entryContainer input {
+        position: absolute;
+        opacity: 0;
+        cursor: pointer;
+        height: 0;
+        width: 0;
+    }
+
+    /* Custom checkbox */
+    #entryCheckbox {
+        display: inline-block;
+        position: relative;
+
+        top: 8px;
+        left: 0;
+        width: 21px;
+        height: 21px;
+
+        background-color: #ffffff;
+        border: 3px solid #7FC4FD;
+        border-radius: 4px;
+    }
+
+    #entryContainer input:checked ~ #entryCheckbox {
+        background-color: #2196F3;
+    }
 </style>
