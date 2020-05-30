9da5dc5dfbe578341176f5c8a8a7467a90ad3809
AntoninThioux <antonin.thioux@gmail.com>
2020-05-30 10:21:06 +0200

    small bug fixed

--- application/src/classes/trucks/TraditionalTruck.js
+++ application/src/classes/trucks/TraditionalTruck.js
@@ -12,20 +12,20 @@ export default class TraditionalTruck extends Truck{
      * @returns {{delivery: number, cost: number, pickup: number}} Object containing; cost, pickup and delivery indexes.
      * Note: If the truck can't transport the good the returned cost is Infinity!
      */
-    getLowestCost(product) {
+    getLowestCost(good) {
         // Keeps track of the best place to added product for the lowest cost.
         let best = {cost: Infinity, pickup: 0, delivery: 0};
 
         // Case the truck can't transport the product, meaning it doesn't fit in the truck.
-        if (product.weight * product.quantity > this.properties.maxPayload ||
-            product.volume * product.quantity > this.properties.volume) return best;
+        if (good.weight * good.quantity > this.properties.maxPayload ||
+            good.volume * good.quantity > this.properties.volume) return best;
 
         // Loop over all orders, that aren't delivery orders.
         let lowestIndex = (this.planManager.getPlanLength() !== 0) ? this.planManager.getIndex() + 1: 0;
         for (let pickup = lowestIndex; pickup <= this.planManager.getPlanLength(); pickup++) {
             if (pickup < this.planManager.getPlanLength() && this.planManager.getOrder(pickup).type === "delivery") continue;
             let delivery = pickup;
-            let cost = this.getCost(product, pickup, delivery);
+            let cost = this.getCost(good, pickup, delivery);
             if (cost < best.cost) best = {cost: cost, pickup: pickup, delivery: delivery};
         }
         return best;
